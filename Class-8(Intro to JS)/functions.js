4977b2b143825042efaf4f9c785f1c23729bdcbd
mrinal1224 <iammrinalkc@gmail.com>
2024-09-30 15:22:01 +0530

    push

---
+++ Class-8(Intro to JS)/functions.js
@@ -0,0 +1,14 @@
+// function serveBeverage(drink){
+//     console.log(drink ,' is Served')
+// }
+
+// serveBeverage('tea') // invokation
+// serveBeverage('coffee')// 
+// serveBeverage('apple juice')
+
+
+function addTwoNumbers(a ,b){
+  console.log(a+b)
+}
+
+addTwoNumbers(2 , 6)
--- Class-8(Intro to JS)/script.js
+++ Class-8(Intro to JS)/script.js
@@ -1,1 +1,39 @@
-console.log('Hello From Vs Code')
+// console.log('Hello From Vs Code')
+
+// variables
+
+var a = 100 // Number
+var b = "Scaler" // String
+var c = true // boolean
+var d = "v" // string
+
+a = 50 // reassignment
+
+// console.log(typeof a)
+// console.log(typeof b)
+// console.log(typeof c)
+// console.log(typeof d)
+
+
+var a = 60
+
+console.log(a)
+
+// let 
+
+let r = 120
+
+r = 70
+
+console.log(r)
+
+// const 
+
+const n = 100
+
+ n = 80
+
+ console.log(n)
+
+
+
