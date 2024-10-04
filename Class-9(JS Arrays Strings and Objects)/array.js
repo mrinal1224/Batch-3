eff93467d5ecde5eca6c95acc0799a72c71ee08d
mrinal1224 <iammrinalkc@gmail.com>
2024-10-04 09:48:23 +0530

    push

--- Class-9(JS Arrays Strings and Objects)/array.js
+++ Class-9(JS Arrays Strings and Objects)/array.js
@@ -1,19 +1,58 @@
-let numbers = [23 ,45,67 ,23, 76]
+// let numbers = [23 ,45,67 ,23, 76]
 
-console.log(numbers)
+// console.log(numbers)
 
-// multiply all elements with 2 and provide results in a new array
+// // multiply all elements with 2 and provide results in a new array
 
-function multiplyBy2(arr){
-    let results = []
-    for(let i=0 ; i<arr.length ; i++){
-        results.push(arr[i]*2)
-    }
-    return results
-}
+// function multiplyBy2(arr){
+//     let results = []
+//     for(let i=0 ; i<arr.length ; i++){
+//         results.push(arr[i]*2)
+//     }
+//     return results
+// }
 
-let finalResults = multiplyBy2(numbers)
-console.log(finalResults)
+// let finalResults = multiplyBy2(numbers)
+// console.log(finalResults)
+
+// Array Methods
+
+//push
+
+let cars = ['Ferrari' , 'BMW' , 'Nano']
+
+// push method
+
+cars.push('Bentley')
+
+console.log(cars)
+
+// pop 
+
+const removedElement = cars.pop()
+console.log(removedElement)
+console.log(cars)
+
+
+// unshift
+
+cars.unshift('Urus')
+
+console.log(cars)
+
+// shift
+
+cars.shift()
+
+console.log(cars)
+
+
+
+
+
+
+
+
 
 
 
