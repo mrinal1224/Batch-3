17dd8ebe292daa46639430703efe0027a1d9cfd6
mrinal1224 <iammrinalkc@gmail.com>
2024-10-09 13:26:25 +0530

    push

---
+++ Class-11(Functional Programming)/pureImpure.js
@@ -0,0 +1,17 @@
+
+
+function sum(a ,b ,c){
+  return a+b+c 
+}
+
+sum(1,2,3)
+
+
+let finalSum1 = sum(2,3,1)
+let finalSum2 = sum(2,3,1)
+let finalSum3 = sum(2,3,1)
+
+
+console.log(finalSum1)// 5
+console.log(finalSum2)// 6
+console.log(finalSum3)// 7
