6c17fdab0073e9c73a6838cfe54ef2c9d0efe2b0
mrinal1224 <iammrinalkc@gmail.com>
2024-10-16 12:02:25 +0530

    push

---
+++ Class-12(Map Filter reduce)/map.js
@@ -0,0 +1,8 @@
+// Exercise1-
+
+// You are given an array , 
+//you have to calucalte sqaure of each element
+//  and return all the 
+//sqaure values in a new array
+
+// let arr1 = [1 ,2 ,3 ,4 ,5]
