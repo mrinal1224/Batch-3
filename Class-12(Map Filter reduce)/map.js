bfa0b8a1af8e2022fbed44f1ce35ce0338523aa3
mrinal1224 <iammrinalkc@gmail.com>
2024-10-16 12:33:20 +0530

    push

--- Class-12(Map Filter reduce)/map.js
+++ Class-12(Map Filter reduce)/map.js
@@ -1,8 +1,42 @@
 // Exercise1-
 
-// You are given an array , 
-//you have to calucalte sqaure of each element
-//  and return all the 
+// You are given an array ,
+//you have to calculate sqaure of each element
+//  and return all the
 //sqaure values in a new array
+// Create a function
 
-// let arr1 = [1 ,2 ,3 ,4 ,5]
+let nums = [1, 2, 3, 4, 5];
+// Impertaive code
+function calculateSqaure(arr) {
+  let newArr = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    newArr.push(arr[i] * arr[i]);
+  }
+  return newArr;
+}
+
+let sqaures = calculateSqaure(nums);
+console.log(sqaures);
+console.log(nums);
+
+// Map
+
+let nums2 = [1, 2, 3, 4, 5];
+
+let mappedSqaures = nums2.map(function (val) {
+  return val * val;
+});
+
+console.log("Sqaure from Map ", mappedSqaures);
+console.log(nums2)
+
+// Excersie - 1
+
+let transcations = [1000 ,2000 ,3000 ,4000 ,5000]
+let inrToDollar = 84
+// Convert transcations in Rs comvert to dollars
+
+
+
